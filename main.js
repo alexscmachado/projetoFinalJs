7d2812b1a51ee96bbceee056169b2bb885588329
Alex Machado <coderaxmcaps@gmail.com>
2023-03-08 19:01:16 -0300

    commit ana

--- main.js
+++ main.js
@@ -5,62 +5,63 @@ function livrosAdd(nome, autor, editora, preco){
     this.preco=preco
 }
 class Adicionar{
+    constructor(){
+        this.cadastroDeLivro = []
+    }
+    adicionarLivro(){
+    
+        let nome = prompt ("Digite o nome do  Título ")      
+        let autor = prompt ("Digite o nome do Autor: ")
+        let editora = prompt ("Digite o nome da Editora: ")
+        let preco = prompt ("Digite o preço: "+"R$:")
+        const novoLivro = new livrosAdd (nome,autor, editora, preco)
 
-constructor(){
-    this.cadastroDeLivro = []
-
-}
-adicionarLivro(){
-   
-    let nome = prompt ("Digite o nome do  Título ")      
-    let autor = prompt ("Digite o nome do Autor: ")
-    let editora = prompt ("Digite o nome da Editora: ")
-    let preco = prompt ("Digite o preço: "+"R$:")
-    const novoLivro = new livrosAdd (nome,autor, editora, preco)
-
-        this.cadastroDeLivro.push(novoLivro)
-      
-}
-
-listarLivros(){
-  
-   for(let cont of this.cadastroDeLivro){
+            this.cadastroDeLivro.push(novoLivro)
         
-        
-        alert("Livros encontrados:" +"\n" + cont.nome +"\n" + "R$" +" "+ cont.preco)
     }
 
-      
+    buscarLivros(busca){
+        let livroNaoEncontrado = true
+        this.cadastroDeLivro.forEach(element => {
+            if(element.nome == busca){
+                livroNaoEncontrado = false
+                alert("Livros encontrados:" +"\n" + element.nome +"\n" + "R$" +" "+ element.preco)
+            } 
+            
+        });
+        if (livroNaoEncontrado == true){
+            alert("Nenhum livro encontrado com o título " + busca) 
+        }
     }
 
+    listarLivros(){
+        let titulosLivros = ""
+        this.cadastroDeLivro.forEach(element => {
+            titulosLivros += element.nome + "\n"
+        });
+        alert("Os livros cadastrados sao:" + "\n" + titulosLivros)
+    }
 
+    removerlivro(indice){
+        this.cadastroDeLivro.splice(indice,1)
+
+    }
+}
     
-}
-removerlivro(indice){
-    this.cadastroDeLivro.splice(indice,1)
 
-}
-buscarLivro(){
-      this.cadastroDeLivro.forEach((nomes) => {
-        alert (this.cadastroDeLivro.nome)
-        
-      });
-}
-}
-    
-
-const sair =0
+const sair = 0
 const cadastrar = 1
 const buscar = 2
-const remover =3
+const remover = 3
+const listar = 4
 
 const escolha = () =>{
     return `
    0 - Sair
    1 - Adicionar Livro
-   2 - Listar Livros
+   2 - Buscar Livros
    3 - remover Livro
-   4 - Editar Editar`
+   4 - Listar Livros`
 }
 
 function livraria(){
@@ -76,15 +77,20 @@ function livraria(){
                 break
 
             case buscar:
-                // let nomeBusca=prompt("Digite o nome do livro que deseja buscar:")
-                add.listarLivros()
-            break
+                let busca = prompt("Digite o livro que deseja buscar")
+                add.buscarLivros(busca)
+                break
+
             case remover:
                 let indice = parseInt( prompt ("Escolha o índice que deseja remover:"))
                 add.removerlivro(indice)
                 break
             
-                case sair:
+            case listar:
+                add.listarLivros()
+                break
+
+            case sair:
                 alert ("até logo")
                 break
             
